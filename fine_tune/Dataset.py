eab37ce2fb968cea7df0e0049a7fb8ace2bcc381
Iris88 <xiwudamengbi@foxmail.com>
2022-09-28 02:36:25 +0800

    Update Dataset.py

--- fine_tune/Dataset.py
+++ fine_tune/Dataset.py
@@ -13,7 +13,7 @@ class FileDataset(Dataset):
             self._total_data = len(f.readlines())
 
     def check_length(self, pairlist):
-        # assert len(pairlist) % 2 == 0
+        assert len(pairlist) % 2 == 0
         max_seq_length = self._max_seq_length - 3
         if len(pairlist) == 2:
             while len(pairlist[0]) + len(pairlist[1]) + 2 > max_seq_length:
@@ -25,10 +25,16 @@ class FileDataset(Dataset):
             q_d_minimum_length = 0
             for i in range(len(pairlist)):
                 q_d_minimum_length += len(pairlist[i]) + 1
+                # print(pairlist[i])
             if q_d_minimum_length > max_seq_length:
-                pairlist.pop(0)
-                pairlist.pop(0)
-                pairlist = self.check_length(pairlist)
+                if(len(pairlist)>2):
+                    pairlist.pop(0)
+                    pairlist.pop(0)
+                    pairlist = self.check_length(pairlist)
+                else:
+                    pairlist[0].pop(0)
+                    pairlist[1].pop(-1)
+        # print(pairlist)
         return pairlist
 
     def anno_main(self, qd_pairs):
@@ -37,78 +43,60 @@ class FileDataset(Dataset):
             qd = self._tokenizer.tokenize(qd)
             all_qd.append(qd)
         all_qd = self.check_length(all_qd)
-        try:
-            history = all_qd[:-2]
-            query_tok = all_qd[-2]
-            doc_tok = all_qd[-1]
-            history_toks = ["[CLS]"]
-            segment_ids = [0]
-            for iidx, sent in enumerate(history):
-                history_toks.extend(sent + ["[eos]"])
-                segment_ids.extend([0] * (len(sent) + 1))
-            query_tok += ["[eos]"]
-            query_tok += ["[SEP]"]
-            doc_tok += ["[eos]"]
-            doc_tok += ["[SEP]"]
-            all_qd_toks = history_toks + query_tok + doc_tok
-            segment_ids.extend([0] * len(query_tok))
-            segment_ids.extend([1] * len(doc_tok))
-            all_attention_mask = [1] * len(all_qd_toks)
-            assert len(all_qd_toks) <= self._max_seq_length
-            while len(all_qd_toks) < self._max_seq_length:
-                all_qd_toks.append("[PAD]")
-                segment_ids.append(0)
-                all_attention_mask.append(0)
-            assert len(all_qd_toks) == len(segment_ids) == len(all_attention_mask) == self._max_seq_length
-            anno_seq = self._tokenizer.convert_tokens_to_ids(all_qd_toks)
-            input_ids = np.asarray(anno_seq)
-            all_attention_mask = np.asarray(all_attention_mask)
-            segment_ids = np.asarray(segment_ids)
-            return input_ids, all_attention_mask, segment_ids
-        except Exception:
-            history = all_qd[:-1]
-            query_tok = all_qd[-1]
-            doc_tok = all_qd[-1]
-            history_toks = ["[CLS]"]
-            segment_ids = [0]
-            for iidx, sent in enumerate(history):
-                history_toks.extend(sent + ["[eos]"])
-                segment_ids.extend([0] * (len(sent) + 1))
-            query_tok += ["[eos]"]
-            query_tok += ["[SEP]"]
-            doc_tok += ["[eos]"]
-            doc_tok += ["[SEP]"]
-            all_qd_toks = history_toks + query_tok + doc_tok
-            segment_ids.extend([0] * len(query_tok))
-            segment_ids.extend([1] * len(doc_tok))
-            all_attention_mask = [1] * len(all_qd_toks)
-            assert len(all_qd_toks) <= self._max_seq_length
-            while len(all_qd_toks) < self._max_seq_length:
-                all_qd_toks.append("[PAD]")
-                segment_ids.append(0)
-                all_attention_mask.append(0)
-            assert len(all_qd_toks) == len(segment_ids) == len(all_attention_mask) == self._max_seq_length
-            anno_seq = self._tokenizer.convert_tokens_to_ids(all_qd_toks)
-            input_ids = np.asarray(anno_seq)
-            all_attention_mask = np.asarray(all_attention_mask)
-            segment_ids = np.asarray(segment_ids)
-            return input_ids, all_attention_mask, segment_ids
+        history = all_qd[:-2]
+        query_tok = all_qd[-2]
+        doc_tok = all_qd[-1]
+            # print(history)
+            # print(query_tok)
+            # print(doc_tok)
+        history_toks = ["[CLS]"]
+        segment_ids = [0]
+        for iidx, sent in enumerate(history):
+            history_toks.extend(sent + ["[eos]"])
+            segment_ids.extend([0] * (len(sent) + 1))
+        query_tok += ["[eos]"]
+        query_tok += ["[SEP]"]
+        doc_tok += ["[eos]"]
+        doc_tok += ["[SEP]"]
+        all_qd_toks = history_toks + query_tok + doc_tok
+        segment_ids.extend([0] * len(query_tok))
+        segment_ids.extend([1] * len(doc_tok))
+        all_attention_mask = [1] * len(all_qd_toks)
+        assert len(all_qd_toks) <= self._max_seq_length
+        while len(all_qd_toks) < self._max_seq_length:
+            all_qd_toks.append("[PAD]")
+            segment_ids.append(0)
+            all_attention_mask.append(0)
+        assert len(all_qd_toks) == len(segment_ids) == len(all_attention_mask) == self._max_seq_length
+        anno_seq = self._tokenizer.convert_tokens_to_ids(all_qd_toks)
+        input_ids = np.asarray(anno_seq)
+        all_attention_mask = np.asarray(all_attention_mask)
+        segment_ids = np.asarray(segment_ids)
+        return input_ids, all_attention_mask, segment_ids
+
 
     def __getitem__(self, idx):
         line = linecache.getline(self._filename, idx + 1)
-        line = line.strip().split("\t")
-        # label = int(line[0])
-        qd_pairs = line[1:]
-        label = 1
-        input_ids, attention_mask, segment_ids = self.anno_main(qd_pairs)
-        batch = {
-            'input_ids': input_ids,
-            'token_type_ids': segment_ids,
-            'attention_mask': attention_mask,
-            'labels': float(label)
-        }
-        return batch
+        line = line.split("\t")
+        try:
+            label = int(line[0])
+            # print(label)
+            qd_pairs = line[1:]
+            input_ids, attention_mask, segment_ids = self.anno_main(qd_pairs)
+            batch = {
+                'input_ids': input_ids,
+                'token_type_ids': segment_ids,
+                'attention_mask': attention_mask,
+                'labels': float(label)
+            }
+            # print(batch)
+            if batch is not None:
+                return batch
+            else:
+                self.__getitem__(idx+1)
+        except Exception as e:
+            print(e)
+            pass
 
     def __len__(self):
         return self._total_data
-
